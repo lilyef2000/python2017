80afcf8b11404851483c40d71580169bc8f48c8f
lilyef2000 <lilyef2000@gmail.com>
2017-10-05 12:59:05 +0800

    10051

---
+++ python动手练.py
@@ -0,0 +1,34 @@
+﻿#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+print u"--------输出从1加到100的结果--------"
+total = 0
+for i in range(1,101):
+    total += i
+print total
+
+print u"--------打印100以内质数--------"
+def judge(x):
+    for i in range(2,x):
+        if x % i == 0:
+            return False
+    return True
+
+for i in range(2,101):
+    if judge(i):
+        print i
+
+print u"--------计算一个文件中每个英文单词出现的次数--------"
+f = open("test.txt")
+lines = f.readlines()
+f.close()
+count = {}
+for line in lines:
+    tokens = line.strip().split(' ')
+    for token in tokens:
+        if token not in count:
+            count[token] = 0
+        count[token] += 1
+
+for word in count:
+    print word,count[word]
